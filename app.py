188bafc6f62ea5acc2675d11a2a8aac133d4b6db
Priyanshu Mishra <priyanshu0171@gmail.com>
2021-07-27 15:54:00 +0530

    Started Development of Detoxify.io

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashbard.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
